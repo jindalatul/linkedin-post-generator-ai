e3fff516e78aedb82ab3ac9b88b7948bbf6955f7
Atul Jindal <er.atul.jindal@gmail.com>
2024-11-09 19:16:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+        function wait(ms)
+        {
+            var start = new Date().getTime();
+            var end = start;
+            while(end < start + ms) {
+                end = new Date().getTime();
+            }
+        }
+        $(document).ready(function(){
+            $("#post-text-editor").hide();
+            
+            $("#Generate-Prompt-Again").click(function(){
+                alert("Generate-Prompt-Again");
+                $("#post-text-editor").hide();
+                $("#loader-spinner").hide();
+                $("#linkedIn-prompt-form").show();
+            });
 
+            $("#post-preview-btn").click(function(){
+                alert("post-preview");
+                $("#post-preview").show();
+            });
+            
+            $("#generate-posts-btn").click(function(){
+                alert("generate-posts-btn");
+                $("#linkedIn-prompt-form").hide();
+                $("#loader-spinner").show();
+                //wait(6000);
+                $("#post-text-editor").show();
+                $("#loader-spinner").hide();
+          });
+          
+        });
