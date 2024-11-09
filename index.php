0b0c87310690c9bc78a317b3510b00c5e2aa47df
Atul Jindal <er.atul.jindal@gmail.com>
2024-11-09 19:17:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="style.js"></script>
 </head>
 <body>
 
